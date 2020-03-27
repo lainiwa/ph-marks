2e16abdaa93bdc1a2384b380770aafb14d31d7b1
lainiwa <kirrik96@gmail.com>
2020-03-27 22:45:18 +0300

    Do major rewrite of the plugin

--- src/ph-marks.zsh
+++ src/ph-marks.zsh
@@ -3,7 +3,12 @@ PH_MARKS_HOME=${PH_MARKS_HOME:-${HOME}/.pornhub}
 PH_MARKS_PARALLELIZATION=${PH_MARKS_PARALLELIZATION:-5}
 
 
-_ph_slugify() {
+.ph_is_viewkey() {
+    [[ ${1} =~ ^ph[0-9a-f]{13}$ ]]
+}
+
+
+.ph_slugify() {
     local argument=$1
     # Substitute spaces with underlines
     argument=${argument// /_}
@@ -13,8 +18,30 @@ _ph_slugify() {
 }
 
 
-_get_video_ids_files() {
-    for file in "${PH_MARKS_HOME}"/*.json; do
-        echo "${file:t:r}"
+.ph_files_viewkeys() {
+    for file in "${PH_MARKS_HOME}"/*.info.json; do
+        echo "${file:t:r:r}"
     done 2>/dev/null
+}
+
+
+.ph_stdin_viewkeys() {
+    if [[ ! -t 0 ]]; then
+        while read -r viewkey; do
+            if ! .ph_is_viewkey "${viewkey}"; then
+                echo "Could not parse \"${viewkey}\"" >&2
+                return 1
+            fi
+            echo "${viewkey}"
+        done </dev/stdin
+    else
+        return 1
+    fi
+}
+
+
+.ph_args_viewkeys() {
+    for arg in "$@"; do
+        echo "${arg}"
+    done
 }
