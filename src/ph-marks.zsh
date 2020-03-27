87f48a703ed8c290de9f2fd783efebcfaf561853
lainiwa <kirrik96@gmail.com>
2020-03-28 02:43:05 +0300

    Add another scheme of possible viewkeys

--- src/ph-marks.zsh
+++ src/ph-marks.zsh
@@ -4,7 +4,10 @@ PH_MARKS_PARALLELIZATION=${PH_MARKS_PARALLELIZATION:-5}
 
 
 .ph_is_viewkey() {
-    [[ ${1} =~ ^ph[0-9a-f]{13}$ ]]
+    # Examples of viewkeys:
+    # * ph5e42e83e8f8c2
+    # * 1270734976
+    [[ ${1} =~ ^ph[0-9a-f]{13}$ || ${1} =~ ^[0-9]{10}$ ]]
 }
 
 
